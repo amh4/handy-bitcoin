d2f1ee81654640d34703be5a3134e6ae72ab2b9e
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-04-19 16:48:14 +0100

    tests passed for displaying profit and loss based on props passed in

--- frontend/src/components/profitAndLoss/profitAndLoss.cy.js
+++ frontend/src/components/profitAndLoss/profitAndLoss.cy.js
@@ -5,4 +5,22 @@ describe("ProfitAndLoss", () => {
     cy.mount(<ProfitAndLoss />);
     cy.get("#profit-loss-container").should("have.text", "Profit/Loss: $0");
   });
+
+  it("loads the component and displays $0 when no trade is live", () => {
+    cy.mount(<ProfitAndLoss startingPrice={0} currentPrice={0} />);
+    cy.get("#profit-loss-container").should("have.text", "Profit/Loss: $0");
+  });
+
+  it("loads the component and displays the profit when a is live and in profit", () => {
+    cy.mount(<ProfitAndLoss startingPrice={30000} currentPrice={32000} />);
+    cy.get("#profit-loss-container").should("have.text", "Profit/Loss: $2,000");
+  });
+
+  it("loads the component and displays the loss when a is live and in loss", () => {
+    cy.mount(<ProfitAndLoss startingPrice={30000} currentPrice={29000} />);
+    cy.get("#profit-loss-container").should(
+      "have.text",
+      "Profit/Loss: $-1,000"
+    );
+  });
 });
--- frontend/src/components/profitAndLoss/profitAndLoss.js
+++ frontend/src/components/profitAndLoss/profitAndLoss.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function ProfitAndLoss() {
+function ProfitAndLoss({ startingPrice, currentPrice }) {
+  const [profitLoss, setProfitLoss] = useState(null);
+
+  useEffect(() => {
+    if (startingPrice != null) {
+      setProfitLoss(currentPrice - startingPrice);
+    } else {
+      setProfitLoss(0);
+    }
+  }, [startingPrice, currentPrice]);
+
   return (
     <div id="profit-loss-container">
-      <p>Profit/Loss: $0</p>
+      <p>Profit/Loss: ${profitLoss?.toLocaleString()}</p>
     </div>
   );
 }
