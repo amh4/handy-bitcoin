a9aaa4d7e3bb425c33a328a7725e0fd2a8c3da79
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-04-23 13:02:40 +0100

    menu loads, is clickable and tutorial item is clickable

--- frontend/src/components/menu/menu.cy.js
+++ frontend/src/components/menu/menu.cy.js
@@ -4,5 +4,8 @@ describe("Menu", () => {
   it("loads the menu", () => {
     cy.mount(<Menu />);
     cy.get(".btn").should("exist");
+    cy.get(".btn").click();
+    cy.get(".offcanvas-body > .btn").click();
+    cy.get(".modal-body");
   });
 });
