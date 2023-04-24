69568b49b7d71e518dde10173198e5ae6d2b1571
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-04-24 17:09:01 +0100

    conditionals for animations fixed. Animations load on profit or loss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,6 @@ import HandTracker from "./components/handTracker/handTracker";
 import UserTutorial from "./components/userTutorial/userTutorial";
 import BitcoinLivePrice from "./components/bitcoinLivePrice/bitcoinLivePrice";
 import ProfitAndLoss from "./components/profitAndLoss/profitAndLoss";
-import TotalProfitLoss from "./components/totalProfitLoss/totalProfitLoss";
-import ProfitAnimation from "./components/profitAnimation/profitAnimation";
-import LossAnimation from "./components/lossAnimation/lossAnimation";
 import LogoNoBackGround from "./components/images/logo-no-background.png";
 import Menu from "./components/menu/menu";
 
@@ -14,21 +11,10 @@ function App() {
   const [currentPrice, setCurrentPrice] = useState();
   const [startingPrice, setStartingPrice] = useState(null);
   const tradeActiveRef = useRef(false);
-  let counter = 0;
-
-  const startAnimation = () => {
-    if (currentProfitLoss < 0) {
-      return false;
-    } else {
-      return true;
-    }
-  };
 
   useEffect(() => {
     if (tradeActiveRef.current && startingPrice === null) {
       setStartingPrice(currentPrice);
-      counter += 1;
-      console.log("counter ", counter);
     } else if (tradeActiveRef.current === false && startingPrice != null) {
       setStartingPrice(null);
     }
@@ -47,12 +33,6 @@ function App() {
         currentPrice={currentPrice}
         tradeActive={tradeActiveRef.current}
       />
-      {!tradeActiveRef.current && counter > 0 && startAnimation() && (
-        <ProfitAnimation />
-      )}
-      {!tradeActiveRef.current && counter > 0 && !startAnimation() && (
-        <LossAnimation />
-      )}
       <HandTracker
         id="hand-tracker"
         onTradeActive={(value) => (tradeActiveRef.current = value)}
