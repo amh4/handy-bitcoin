faae2df9a43268e66f08e23947dd40e37f16cb99
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-05-02 16:04:05 +0100

    created seeds, added to test db

---
+++ api/user_database/user_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username, email, password)
+VALUES
+  ('John Smith', 'john.doe@example.com', 'password123'),
+  ('Jane Doe', 'jane.doe@example.com', 'password456'),
+  ('Bob Smith', 'bob.smith@example.com', 'password789');
